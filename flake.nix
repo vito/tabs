1935ccb2c4725de65d05dc2c0e0eccc185c886ee
Alex Suraci <suraci.alex@gmail.com>
2022-07-30 20:47:04 -0400

    add nixery, use it for gh and wget images

--- flake.nix
+++ flake.nix
@@ -5,12 +5,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    rec {
-      packages.x86_64-linux.gh = pkgs.callPackage ./img/gh.nix { };
-      packages.x86_64-linux.wget = pkgs.callPackage ./img/wget.nix { };
-    };
+  outputs =
+    { self
+    , nixpkgs
+    }: { };
 }
